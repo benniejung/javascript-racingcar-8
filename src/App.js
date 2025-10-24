0aafbee61a96104219b672527c1f60f6fe6da463
binniejung <ybe1027@naver.com>
2025-10-25 00:56:25 +0900

    feat(App): 사용자 입력 유효성 검증 로직 추가

--- src/App.js
+++ src/App.js
@@ -6,8 +6,33 @@ class App {
     const input = await Console.readLineAsync(
       "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n"
     );
+    Console.print(input);
+    if (isNaN(input)) {
+      throw new Error("[ERROR] 시도할 횟수는 숫자값이 아닙니다.");
+    }
+    if (!input.includes(",")) {
+      throw new Error("[ERROR] 자동차 이름은 쉼표(,)로 구분해야 합니다.");
+    }
     racingCars = input.split(",");
+    if (racingCars.length < 2) {
+      throw new Error("[ERROR] 자동차 개수는 최소 2개 이상이어야 합니다.");
+    }
+    // 자동차 이름 5자 초과 체크
+    racingCars.map((car) => {
+      if (car.length > 5) {
+        throw new Error("[ERROR] 자동차 이름은 5자 이하로 제한되어 있습니다.");
+      }
+    });
+    // 자동차 이름 중복 체크
+    racingCars.map((car) => {
+      if (racingCars.filter((c) => c === car).length > 1) {
+        throw new Error("[ERROR] 자동차 이름이 중복되었습니다.");
+      }
+    });
     tryCount = await Console.readLineAsync("시도할 횟수는 몇 회인가요?\n");
+    if (isNaN(tryCount)) {
+      throw new Error("[ERROR] 시도할 횟수는 숫자값이 아닙니다.");
+    }
   }
 }
 
