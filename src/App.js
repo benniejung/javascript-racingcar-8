1588cef8f218f0c50c98fa15e15cdb0f9e777442
binniejung <ybe1027@naver.com>
2025-10-25 22:50:09 +0900

    refactor(App): RacingCar 클래스 추가 및 로직 개선
    - 클래스 메서드 기반 게임 루프 구현

--- src/App.js
+++ src/App.js
@@ -1,8 +1,29 @@
 import { Console, MissionUtils } from "@woowacourse/mission-utils";
+
+class RacingCar {
+  constructor(name) {
+    this.name = name; // 자동차 이름
+    this.moveCount = 0; // 자동차 전진횟수
+  }
+  move() {
+    const randomNumber = MissionUtils.Random.pickNumberInRange(0, 9);
+    if (randomNumber >= 4) {
+      this.moveCount += 1;
+    }
+  }
+  print() {
+    Console.print(`${this.name} : ${"-".repeat(this.moveCount)}`);
+  }
+  getMoveCount() {
+    return this.moveCount;
+  }
+  getName() {
+    return this.name;
+  }
+}
 class App {
   async run() {
-    let racingCars = []; // 경주할 자동차 이름을 저장할 배열
-    let tryCount = 0; // 시도할 횟수를 저장할 변수
+    let tryCount = 0;
     const input = await Console.readLineAsync(
       "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n"
     );
@@ -12,7 +33,7 @@ class App {
     if (!input.includes(",")) {
       throw new Error("[ERROR] 자동차 이름은 쉼표(,)로 구분해야 합니다.");
     }
-    racingCars = input.split(",");
+    const racingCars = input.split(",").map((name) => new RacingCar(name));
     if (racingCars.length < 2) {
       throw new Error("[ERROR] 자동차 개수는 최소 2개 이상이어야 합니다.");
     }
@@ -28,19 +49,15 @@ class App {
         throw new Error("[ERROR] 자동차 이름이 중복되었습니다.");
       }
     });
-    let racinCarsStatus = new Array(racingCars.length).fill(0); // 경주할 자동차 상태를 저장할 배열
     tryCount = await Console.readLineAsync("시도할 횟수는 몇 회인가요?\n");
     if (isNaN(tryCount)) {
       throw new Error("[ERROR] 시도할 횟수는 숫자값이 아닙니다.");
     }
 
     for (let i = 0; i < tryCount; i++) {
-      racingCars.map((car, index) => {
-        const randomNumber = MissionUtils.Random.pickNumberInRange(0, 9);
-        if (randomNumber >= 4) {
-          racinCarsStatus[index] += 1;
-        }
-        Console.print(`${car} : ${"-".repeat(racinCarsStatus[index])}`);
+      racingCars.map((car) => {
+        car.move();
+        car.print();
       });
       Console.print("\n");
     }
