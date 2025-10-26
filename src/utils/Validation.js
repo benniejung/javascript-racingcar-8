4053af38be0db430cc7753ba7babb4f88e8e9cb3
binniejung <ybe1027@naver.com>
2025-10-26 14:56:33 +0900

    refactor(validation): 에러 메시지 상수화 및 validation.js 코드 개선
    - constants.js 파일 생성 및 ERROR_MESSAGE 상수 정의
    
    validation.js에서 에러 메시지를 상수로 대체

--- src/utils/Validation.js
+++ src/utils/Validation.js
@@ -1,31 +1,32 @@
+import { ERROR_MESSAGE } from "./constants.js";
 const validateInputCarNames = (inputCarNames) => {
   if (inputCarNames.length === 0) {
-    throw new Error("[ERROR] 자동차 이름을 입력해주세요.");
+    throw new Error(ERROR_MESSAGE.INPUT_CAR_NAMES);
   }
   if (!inputCarNames.includes(",")) {
-    throw new Error("[ERROR] 자동차 이름은 쉼표(,)로 구분해야 합니다.");
+    throw new Error(ERROR_MESSAGE.INPUT_CAR_NAMES_SEPARATOR);
   }
 };
 
 const validateCarNames = (racingCars) => {
   if (racingCars.some((car) => car.name != String(car.name))) {
-    throw new Error("[ERROR] 자동차 이름은 문자열이어야 합니다.");
+    throw new Error(ERROR_MESSAGE.INVALID_CAR_NAME);
   }
   if (racingCars.length < 2) {
-    throw new Error("[ERROR] 자동차 개수는 최소 2개 이상이어야 합니다.");
+    throw new Error(ERROR_MESSAGE.INVALID_CAR_COUNT);
   }
   if (racingCars.some((car) => car.name.length > 5 || car.name.length === 0)) {
-    throw new Error("[ERROR] 자동차 이름은 5자 이하이거나 공백일 수 없습니다.");
+    throw new Error(ERROR_MESSAGE.INVALID_CAR_NAMES_LENGTH);
   }
   const uniqueNames = new Set(racingCars.map((car) => car.name));
   if (uniqueNames.size !== racingCars.length) {
-    throw new Error("[ERROR] 자동차 이름이 중복되었습니다.");
+    throw new Error(ERROR_MESSAGE.INVALID_DUPLICATE_CAR_NAMES);
   }
 };
 
 const validateTryCount = (tryCount) => {
-  if (isNaN(tryCount)) {
-    throw new Error("[ERROR] 시도할 횟수는 숫자값이 아닙니다.");
+  if (tryCount != Number(tryCount)) {
+    throw new Error(ERROR_MESSAGE.INPUT_TRY_COUNT);
   }
 };
 
---
+++ src/utils/constants.js
@@ -0,0 +1,12 @@
+const ERROR_MESSAGE = {
+  INPUT_CAR_NAMES: "[ERROR] 자동차 이름을 입력해주세요.",
+  INPUT_TRY_COUNT: "[ERROR] 시도할 횟수는 숫자값이 아닙니다.",
+  INPUT_CAR_NAMES_SEPARATOR: "[ERROR] 자동차 이름은 쉼표(,)로 구분해야 합니다.",
+  INVALID_CAR_NAME: "[ERROR] 자동차 이름은 문자열이어야 합니다.",
+  INVALID_CAR_COUNT: "[ERROR] 자동차 개수는 최소 2개 이상이어야 합니다.",
+  INVALID_CAR_NAMES_LENGTH:
+    "[ERROR] 자동차 이름은 5자 이하이거나 공백일 수 없습니다.",
+  INVALID_DUPLICATE_CAR_NAMES: "[ERROR] 자동차 이름이 중복되었습니다.",
+};
+
+export { ERROR_MESSAGE };
