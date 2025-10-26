f0dfe3df8adb33a23c04da5cc5684ea38c8d52dd
binniejung <ybe1027@naver.com>
2025-10-26 14:42:42 +0900

    refactor(controller): MVC 패턴 적용을 위한 Controller 클래스 추가
    - 게임 플로우를 Controller에서 관리

---
+++ src/controller/controller.js
@@ -0,0 +1,24 @@
+import {
+  validateInputCarNames,
+  validateCarNames,
+  validateTryCount,
+} from "../utils/Validation.js";
+
+export default class Controller {
+  constructor(view, model) {
+    this.view = view;
+    this.model = model;
+  }
+  async gameStart() {
+    const inputCarNames = await this.view.inputCarNames();
+    validateInputCarNames(inputCarNames);
+    const racingCars = inputCarNames
+      .split(",")
+      .map((name) => this.model.createRacingCars(name));
+    validateCarNames(racingCars);
+    const inputTryCount = await this.view.inputTryCount();
+    validateTryCount(inputTryCount);
+    this.view.printRacingProcess(racingCars, inputTryCount);
+    this.view.printRacingResult(racingCars);
+  }
+}
